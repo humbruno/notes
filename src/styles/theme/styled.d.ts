a979f9f02e38161aae9dd6ecf2a03b1fba526240
Bruno Santos <>
2023-02-15 21:30:07 +0000

    chore: fix theme typing

--- src/styles/theme/styled.d.ts
+++ src/styles/theme/styled.d.ts
@@ -1,38 +1,6 @@
-import 'styled-components';
-import theme from './index';
+import {} from 'styled-components';
+import { theme } from './theme';
 
 declare module 'styled-components' {
-  export interface DefaultTheme {
-    colors: {
-      primary: {
-        rose: string;
-        white: string;
-        midnight: string;
-        creamWhite: string;
-      };
-      semantic: {
-        red: string;
-        green: string;
-      };
-      post: {
-        lightYellow: string;
-        redOrange: string;
-        lilac: string;
-        greenCyan: string;
-        lightCyan: string;
-      };
-      grays: {
-        gray900: string;
-        gray800: string;
-        gray600: string;
-        gray400: string;
-        gray300: string;
-      };
-    };
-    fontWeights: {
-      regular: string;
-      medium: string;
-      bold: string;
-    };
-  }
+  export interface DefaultTheme extends theme {}
 }
